eb7761a9c43bc255212918e89c3c4fff4d434a07
Amine Takha <amine@uzerly.fr>
2022-04-03 17:25:57 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there
+        </p>
+        <p>
+          Serving from Nginx
         </p>
         <a
           className="App-link"
